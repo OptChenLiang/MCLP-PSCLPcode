9e77a8336a9da551ab12882183173ce8f6d13252
ShengjieChen <55678029+ShengjieChen@users.noreply.github.com>
2021-08-23 23:37:47 +0800

    Update MCLP_BEN.h

--- PREBEN/src_MCLP/MCLP_BEN.h
+++ PREBEN/src_MCLP/MCLP_BEN.h
@@ -9,15 +9,15 @@
 using namespace std;
 
 
-/*****************************************************************/
+//Build Benders master problem
 void build_model_DFL_BEN_2(instance *inst);
 /*****************************************************************/
 
-/*****************************************************************/
+//Solve MCLP using Benders decomposition
 void solve_model_DFL_BEN_2(instance *inst, mystr* inst1);
 /*****************************************************************/
 
-/*****************************************************************/
+//Close CPLEX environment
 void clean_model_DFL_BEN_2(instance *inst);
 /*****************************************************************/
 
